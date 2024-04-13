64fae83df0299e36d6f1616286e99368ede75b18
pawel123 <pawelek78-30@o2.pl>
2024-04-13 11:57:43 +0200

    modyfikacja 12.04.2024

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+ 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +21,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/post', [PostController::class, 'index']);
+Route::post('/post', [PostController::class, 'wyslij'])->name('dane');
+Route::post('/usun/{post}', [PostController::class, 'destroy'])->name('delete');
 require __DIR__.'/auth.php';
+
+
