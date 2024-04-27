1788ef4b03ff0199f7c1851167ed146b0a22b735
pawel123 <pawelek78-30@o2.pl>
2024-04-27 17:18:46 +0200

    tailwind install

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\ContactsController;
+ 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,11 +20,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
 });
-
-Route::get('/post', [PostController::class, 'index']);
-Route::post('/post', [PostController::class, 'wyslij'])->name('dane');
-Route::post('/usun/{post}', [PostController::class, 'destroy'])->name('delete');
+Route::get('/contakt', [ContactsController::class, 'index']);
+Route::post('contakt_message', [ContactsController::class, 'store'])->name('send_post');
+ 
+ 
+ 
 require __DIR__.'/auth.php';
 
 
